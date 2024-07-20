1e498415ccaf13dd9165a16fddba7a3c61c0591c
Татьяна Татаренко <tatatatarenko38@gmail.com>
2024-07-20 12:14:06 +0300

    ts1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import "./App.css";
+
+// function App() {
+//   let age: number = 25;
+//   let height: number = 1.75;
+
+//   const name: string = "Vacya";
+//   const message: string = "hello";
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>{name} {message}</p>
+
+//         <p>
+//           {age} {height}
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+//коли другий аргумент необов'язковий, а функція не стрілкова
+// function getSpace2(
+//   name: string,
+//   callback: (value: string, test?: number) => string): string {
+//   return callback(name);
+// }
+
+// function App() {
+//   const getSpace = (num: number): string => `${num * 4}px`;
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>{getSpace(4)}</p>
+//       </header>
+//     </div>
+//   )
+// }
+
+// void - відсутність повернення значення функції
+// function App() {
+//   const handleChange = (value: number): void => {
+
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>hrllo</p>
+//       </header>
+//     </div>
+//   )
+// }
+
+//об'єкт
 function App() {
+  let data: object = {};
+
+  let person: { name: string; age: number; isStudent: boolean } = {
+    name: "fffff",
+    age: 25,
+    isStudent: true,
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{person.name}</p>
       </header>
     </div>
   );
